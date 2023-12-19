b941a6ad59468fc5ababe0c3a2b1d12c88c316ec
uday <y.udaykanth@gmail.com>
2023-12-19 20:42:33 +0530

    v

--- 04.functions.sh
+++ 04.functions.sh
@@ -7,3 +7,28 @@ SCRIPT_NAME=$0
 LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
 echo "$DATE,$SCRIPT_NME,$LOGFILE"
 echo $0
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+VALIDATE(){
+    #$1 --> it will receive the argument1
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: Please run this script with root access"
+    exit 1
+# else
+#     echo "INFO:: You are root user"
+fi
+yum install mysql -y &>>$LOGFILE
+VALIDATE $? "Installing MySQL"
