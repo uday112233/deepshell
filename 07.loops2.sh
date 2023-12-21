12c2b16ac12fa86cabbb3485d9fc3875a5cc7222
uday <y.udaykanth@gmail.com>
2023-12-21 07:28:55 +0530

    2nd commit

--- 07.loops2.sh
+++ 07.loops2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DATE=$(date +%F)
-LOGSDIR=/home/centos/shellscript-logs
+LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
 # /home/centos/shellscript-logs/script-name-date.log
 SCRIPT_NAME=$0
 LOGFILE=$LOGSDIR/$0-$DATE.log
